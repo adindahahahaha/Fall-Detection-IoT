5f5ce8d80c245d7d8c33afbfbae0470e015c4f11
hadelkim3 <153834598+hadelkim3@users.noreply.github.com>
2024-11-28 21:36:37 +0700

    Update firebase_config.js

--- Cloud_Computing/firebase_config/firebase_config.js
+++ Cloud_Computing/firebase_config/firebase_config.js
@@ -2,7 +2,7 @@
 export const firebaseConfig = {
     apiKey: "<your-api-key>",
     authDomain: "<your-auth-domain>",
-    projectId: "<your-project-id>",
+    projectId: "fall-detection-iot-e3062",
     storageBucket: "<your-storage-bucket>",
     messagingSenderId: "<your-messaging-sender-id>",
     appId: "<your-app-id>"
